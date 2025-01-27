7f23190466fe4dd52fdec48a678f24d419ca4c82
Jacob Guillemette <NinteMars@proton.me>
2025-01-26 20:46:05 -0500

    Add the current condition to the display
    Sets up the condition for the display, with a smaller font, adding it
    right below the temperature.

--- draw_temperature.py
+++ draw_temperature.py
@@ -2,23 +2,38 @@ import sys
 
 from inky.auto import auto
 from PIL import Image, ImageFont, ImageDraw
+from current_weather import CurrentWeather
 
 class DisplayTemperature:
-    def __init__(self, temp: int):
-        self._temp: str = f"{str(temp)}°"
+    def __init__(self, cur_weather: CurrentWeather):
+        self._temp: str = f"{str(cur_weather.get_current_temp())}°"
+        self._condition: str = cur_weather.get_current_condition()
     
     def draw_temp(self) -> None:
+        # Initialize the display
         inky_display = auto()
         inky_display.set_border(inky_display.WHITE)
 
         img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
         draw = ImageDraw.Draw(img)
 
-        font = ImageFont.truetype("FredokaOne-Regular.ttf", 50)
+        # Add the main temperature to the display
+
+        font = ImageFont.truetype("FredokaOne-Regular.ttf", 60)
         _, _, w, h = font.getbbox(self._temp)
         x = 20
         y = 20
 
         draw.text((x, y), self._temp, inky_display.BLACK, font)
+
+        # Add the current condition
+
+        condition_font = ImageFont.truetype("FredokaOne-Regular.ttf", 24)
+        _, _, w, h, = font.getbbox(self._condition)
+        x = 20
+        y = 70
+
+        draw.text((x, y), self._condition, inky_display.BLACK, condition_font)
+
         inky_display.set_image(img)
         inky_display.show()
