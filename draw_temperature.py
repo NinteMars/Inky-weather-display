8623b2555b9967d65be31f4520d046295c5e3b8c
Jacob Guillemette <NinteMars@proton.me>
2025-01-26 21:40:30 -0500

    Fix placement of draw for slash
    Draw for slash is currently after putting the maximum temperature,
    messing up formatting. Adds the draw to before the maximum temperature
    is drawn with updated x and y positions.

--- draw_temperature.py
+++ draw_temperature.py
@@ -56,12 +56,12 @@ class DisplayWeather:
         x = x + right
         y = y + 12
         left, top, right, bottom = min_max_font.getbbox("/")
+        draw.text((x, y), '/', inky_display.BLACK, min_max_font)
 
         x = x + right
         y = y + 12
         draw.text((x, y), self._max, inky_display.BLACK, min_max_font)
 
-        draw.text((x, y), '/', inky_display.BLACK, min_max_font)
 
         inky_display.set_image(img)
         inky_display.show()
