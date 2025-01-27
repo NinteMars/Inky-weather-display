e291e6aae1955deb41f816e37a399cd0816ac9a1
Jacob Guillemette <NinteMars@proton.me>
2025-01-26 20:47:44 -0500

    Update main to new method calls
    Updates main to use new method calls (Sending the CurrentWeather object
    to DisplayTemperature instead of just the temperature).
    
    Also prints the condition to allow for testing on personal machine that
    the condition is working.

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from draw_temperature import DisplayTemperature
 
 def main() -> None:
     test: CurrentWeather = CurrentWeather('01854')
+    test.refresh()
     temp = test.get_current_temp()
     print(f"The current temperature in Lowell is {temp}")
-    disp_temp: DisplayTemperature = DisplayTemperature(temp)
+    print(f"The current condition is {test.get_current_condition()}")
+    disp_temp: DisplayTemperature = DisplayTemperature(test)
     disp_temp.draw_temp()
 
 
